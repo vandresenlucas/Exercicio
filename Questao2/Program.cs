b2f9c0994ef0c3bc3746b811ff80451656b2701f
Lucas <lvs.vandresen@gmail.com>
2025-03-08 13:58:56 -0300

    Solving question 2

--- Questao2/Program.cs
+++ Questao2/Program.cs
@@ -2,17 +2,17 @@
 
 public class Program
 {
-    public static void Main()
+    public static async Task Main()
     {
         string teamName = "Paris Saint-Germain";
         int year = 2013;
-        int totalGoals = getTotalScoredGoals(teamName, year);
+        int totalGoals = await getTotalScoredGoals(teamName, year);
 
         Console.WriteLine("Team "+ teamName +" scored "+ totalGoals.ToString() + " goals in "+ year);
 
         teamName = "Chelsea";
         year = 2014;
-        totalGoals = getTotalScoredGoals(teamName, year);
+        totalGoals = await getTotalScoredGoals(teamName, year);
 
         Console.WriteLine("Team " + teamName + " scored " + totalGoals.ToString() + " goals in " + year);
 
@@ -21,10 +21,64 @@ public class Program
         // Team Chelsea scored 92 goals in 2014
     }
 
-    public static int getTotalScoredGoals(string team, int year)
+    public static async Task<int> getTotalScoredGoals(string team, int year)
     {
-        
-        return 0;
-    }
 
+        using (HttpClient client = new HttpClient())
+        {
+            int totalGoals = 0;
+            int currentPage = 1;
+            int totalPages = 1;
+
+            while (currentPage <= totalPages)
+            {
+                var apiUrl = $"https://jsonmock.hackerrank.com/api/football_matches?team1={team}&year={year}&page={currentPage}";
+
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonResponse = await response.Content.ReadAsStringAsync();
+                    dynamic result = JsonConvert.DeserializeObject(jsonResponse);
+
+                    if (currentPage == 1) 
+                        totalPages = result.total_pages;
+
+                    foreach (var match in result.data)
+                    {
+                        totalGoals += int.Parse(match.team1goals.ToString());
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Erro ao acessar API: " + response.StatusCode);
+                    return 0;
+                }
+
+                apiUrl = $"https://jsonmock.hackerrank.com/api/football_matches?team2={team}&year={year}&page={currentPage}";
+
+                response = await client.GetAsync(apiUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonResponse = await response.Content.ReadAsStringAsync();
+                    dynamic result = JsonConvert.DeserializeObject(jsonResponse);
+
+                    foreach (var match in result.data)
+                    {
+                        totalGoals += int.Parse(match.team2goals.ToString());
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Erro ao acessar API: " + response.StatusCode);
+                    return 0;
+                }
+
+                currentPage++;
+            }
+
+            return totalGoals;
+        }
+    }
 }
