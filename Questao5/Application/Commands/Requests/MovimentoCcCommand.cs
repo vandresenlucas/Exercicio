54a32942381324590077d9adbece47b44a3721d5
Lucas <lvs.vandresen@gmail.com>
2025-03-11 12:49:54 -0300

    Including swagger descriptions

--- Questao5/Application/Commands/Requests/MovimentoCcCommand.cs
+++ Questao5/Application/Commands/Requests/MovimentoCcCommand.cs
@@ -1,13 +1,22 @@
 ﻿using MediatR;
 using Questao5.Domain.Entities.Movimento;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace Questao5.Application.Commands.Requests
 {
+    [SwaggerSchema(Description = "Representa os dados para adicionar uma movimentação.")]
     public class MovimentoCcCommand : IRequest<Result>
     {
+        [SwaggerSchema("Identificação da requisição.")]
         public string ChaveIdempotencia { get; set; }
+
+        [SwaggerSchema("Identificação da conta corrente.")]
         public string IdContaCorrente { get; set; }
+
+        [SwaggerSchema("Valor a ser movimentado.")]
         public decimal Valor { get; set; }
+
+        [SwaggerSchema("tipo da movimentação (C = Credito, D = Débito)")]
         public string TipoMovimento { get; set; }
 
         public static implicit operator Movimento(MovimentoCcCommand command)
